31e2d869d4147ae633e17a2d63bf7448e5113c99
Mostafa AbdElmogod <mostafa-for-work@hotmail.com>
2024-01-10 13:58:49 +0200

    media query

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-const navLinks = document.querySelectorAll('nav a');
+// menu bar
+let bar = document.getElementById("bar"),
+  menu = document.getElementById("menu-screen"),
+  blure = document.getElementById("blure")
+  x = document.getElementById("x");
+
+bar.addEventListener("click", () => {
+  blure.style.display = "inline"
+  menu.style.cssText = "visibility: visible;";
+});
+
+x.addEventListener("click", () => {
+  blure.style.display = "none"
+  menu.style.cssText = "display:none;";
+});
+
+// actve nav links and section
+const navLinks = document.querySelectorAll("nav a");
 window.addEventListener("scroll", () => {
   const fromTop = window.scrollY;
   navLinks.forEach((links) => {
@@ -7,18 +24,19 @@ window.addEventListener("scroll", () => {
       section.offsetTop <= fromTop &&
       section.offsetTop + section.offsetHeight > fromTop
     ) {
-        links.classList.add('active');
+      links.classList.add("active");
     } else {
-        links.classList.remove('active');
+      links.classList.remove("active");
     }
   });
 });
 
-var typed = new Typed('.type', {
-    strings: ["Instructor.", "Front-End Dev." , "Back-End Dev." ],
-    typeSpeed: 100,
-    backSpeed :100,
-    backDelay:1000,
-    loop:true
-  });
-  
+// type js
+var typed = new Typed(".type", {
+  strings: ["Instructor.", "Front-End Dev.", "Back-End Dev."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
