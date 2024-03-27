8cd541eef4094b6a36a2d8ebb54084a95b4be535
Mostafa AbdElmogod <mostafa-for-work@hotmail.com>
2024-03-27 21:01:03 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".type", {
+    strings: ["Instructor.", "Web Developer.", "Wordpress developer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
