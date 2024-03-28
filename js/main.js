269bbf0b69fef72335797c71ace4d01aad4090d2
Mostafa AbdElmogod <mostafa-for-work@hotmail.com>
2024-03-28 05:42:43 +0200

    update

--- js/main.js
+++ js/main.js
@@ -15,27 +15,44 @@ let main = document.getElementById("main"),
   contentLeftSpe = document.querySelector(".info-top>div:last-child"),
   social = document.querySelector(".social"),
   hire = document.querySelector(".hire"),
-  navIcon = document.querySelectorAll(".nav-icon>i");
+  title = document.getElementsByClassName("title")[0],
+  navIcon = document.querySelectorAll(".nav-icon>a>i");
 
-  // active nav
-
+//about animation && active nav
 for (let i = 0; i < navIcon.length; i++) {
   navIcon[i].addEventListener("click", () => {
     for (let L = 0; L < navIcon.length; L++) {
       navIcon[L].classList.remove("active");
     }
     navIcon[i].classList.add("active");
+
+    if (navIcon[1].classList.contains("active")) {
+        main.classList.add("main-left-slide");
+        infoTop.classList.add("left-slide");
+        infoImg.classList.add("img-left-slide");
+        contentLeftName.classList.add("f-s");
+        contentLeftSpe.classList.add("d-n");
+        social.classList.add("social-left");
+        hire.classList.add("d-n");
+        title.textContent = "About Me"
+    }else{
+      main.classList.remove("main-left-slide");
+      infoTop.classList.remove("left-slide");
+      infoImg.classList.remove("img-left-slide");
+      contentLeftName.classList.remove("f-s");
+      contentLeftSpe.classList.remove("d-n");
+      social.classList.remove("social-left");
+      hire.classList.remove("d-n");
+    }
   });
 }
-//about animation
 
-if (navIcon[1].classList.contains("active")) {
-  main.classList.add("main-left-slide");
-  infoTop.classList.add("left-slide");
-  infoImg.classList.add("img-left-slide");
-  contentLeftName.classList.add("f-s");
-  contentLeftSpe.classList.add("d-n");
-  social.classList.add("social-left");
-  hire.classList.add("d-n");
-}
+navIcon[0].addEventListener("click" , ()=>{
+  title.textContent = "who am i"
+})
+
+
+
+
+
 
