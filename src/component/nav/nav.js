f7bba437a0c16efbeefce56434cd1e5e7c000ad7
Mostafa AbdElmogod <mostafa-for-work@hotmail.com>
2024-05-13 06:15:05 +0200

    edit

--- src/component/nav/nav.js
+++ src/component/nav/nav.js
@@ -1,5 +1,9 @@
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import AccountTreeOutlinedIcon from '@mui/icons-material/AccountTreeOutlined';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 import './nav.css';
 
 const Nav = ()=>{
@@ -7,6 +11,10 @@ const Nav = ()=>{
         <div className="nav">
            <HomeOutlinedIcon />
            <PersonOutlineOutlinedIcon />
+           <BusinessCenterOutlinedIcon />
+           <AccountTreeOutlinedIcon/>
+           <ArticleOutlinedIcon />
+           <MailOutlineOutlinedIcon />
         </div>
     )
 }
