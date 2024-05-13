54bdd16bc231aa126ae5d720931e66d695571577
Mostafa AbdElmogod <mostafa-for-work@hotmail.com>
2024-05-13 05:49:47 +0200

    update

--- src/component/nav/nav.js
+++ src/component/nav/nav.js
@@ -1,9 +1,12 @@
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import './nav.css';
 
 const Nav = ()=>{
     return (
         <div className="nav">
-
+           <HomeOutlinedIcon />
+           <PersonOutlineOutlinedIcon />
         </div>
     )
 }
